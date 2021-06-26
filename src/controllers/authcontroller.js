845b58218b1fc9abce691848e0e4181774e58ef5
HectorMu <42363909+HectorMu@users.noreply.github.com>
2021-06-26 10:06:35 -0500

    Update authcontroller.js

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -1,5 +1,8 @@
 const authController = {}
+const { urlencoded } = require('express')
 const passport = require('passport')
+
+
 
 authController.signUp = passport.authenticate('signup',{
     successRedirect:'/profile',
@@ -7,11 +10,28 @@ authController.signUp = passport.authenticate('signup',{
     failureFlash:true
 })
 
-authController.login = passport.authenticate('local.login',{
-    successRedirect:'/profile',
-    failureRedirect:'/login',
-    failureFlash:true
-})
+//this controller determines if there's is an user and then redirect that user to
+//a route saved in the database, of this way user can choose what view will pop up at login
+//this can be use too redirect users depending his roles in the system
+//time to make and understand this: 10 hours since 4 pm to 2 am
+authController.login = (req, res, next)=>{
+    passport.authenticate('local.login',(err, user)=>{
+        if(!user){
+            return res.redirect('/login')
+        }
+        req.logIn(user,(err)=>{
+            return res.redirect('/'+req.user.startscreen)
+        })
+    })(req, res, next)
+}
+
+// authController.login = passport.authenticate('local.login',{
+//       successRedirect:'/profile',
+//       failureRedirect:'/login',
+//       failureFlash:true
+// })
+
+
 
 authController.logout = (req, res, next)=>{
     req.logOut();
