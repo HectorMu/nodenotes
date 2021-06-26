5485b2a8c6d859392cc8b6048d4a58ce18942d29
HectorMu <42363909+HectorMu@users.noreply.github.com>
2021-06-26 11:41:24 -0500

    Update notesRoutes.js

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -5,10 +5,13 @@ const auth = require('../lib/auth')
 const notesController = require ('../controllers/notesController')
 
 router.get('/mynotes', auth.isLoggedIn, notesController.listNotes)
+router.get('/mynotes/normal', auth.isLoggedIn, notesController.listNormalNotes)
+router.get('/mynotes/important', auth.isLoggedIn, notesController.listImportantNotes)
+router.get('/mynotes/veryimportant', auth.isLoggedIn, notesController.listVeryImportantNotes)
 router.post('/savenote', auth.isLoggedIn, notesController.saveNote)
 router.get('/editnote/:idnote',auth.isLoggedIn,  notesController.renderEditNote)
 router.post('/editnote/:idnote', auth.isLoggedIn, notesController.editNote)
 router.get('/deletenote/:idnote', auth.isLoggedIn, notesController.deleteNote)
 router.post('/notesearch/', auth.isLoggedIn, notesController.searchNote)
-
+ 
 module.exports = router
