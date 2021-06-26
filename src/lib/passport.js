7ad25cc4436bba2f3a677bde446052bedf6b8d3c
HectorMu <42363909+HectorMu@users.noreply.github.com>
2021-06-26 10:07:12 -0500

    Update passport.js

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -31,18 +31,53 @@ passport.use('signup', new passportLocal({
     passwordField: "pass",
     passReqToCallback: true,
 }, async(req, email, pass,  done)=>{
+const passport = require('passport')
+const passportLocal = require('passport-local').Strategy
+const helpers = require('../lib/helpers')
+const Dbpool = require('../database')
+
+
+passport.use('local.login',new passportLocal({
+    usernameField:"email",
+    passwordField:"pass",
+    passReqToCallback: true,
+}, async(req, email, pass, done)=>{
+    const rows = await Dbpool.query("select * from users where email = ?",[email])
+    if(rows.length >0){
+        const user = rows[0]
+        const validPass = await helpers.validatePass(pass,user.pass)
+        if(validPass){
+            done(null, user)
+        }else{
+            done(null, false, req.flash("error_msg", "Incorrect password"))
+        }
+    }else{
+        return done(null, false,req.flash("error_msg", "The user doesn't exists"))
+    }
+}
+))
+
+
+
+passport.use('signup', new passportLocal({
+    usernameField: "email",
+    passwordField: "pass",
+    passReqToCallback: true,
+}, async(req, email, pass,  done)=>{
     const verifiymail = await Dbpool.query("select * from users where email = ?",[email])
     if(verifiymail.length==0){
         if(pass.length>=8){     
             const {firstname, lastname} = req.body
             const privileges = "user"
+            const startscreen = "profile"
             console.log(req.body)
             let newUser = {
                 firstname,
                 lastname,
                 email,
                 pass,
-                privileges
+                privileges,
+                startscreen
             }
             newUser.pass = await helpers.encryptPass(pass);
             //saving the user in the db
