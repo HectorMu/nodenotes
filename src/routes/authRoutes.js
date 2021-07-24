4e3c86560523aad430e80c8760a6bd694d886dc4
HectorMu <42363909+HectorMu@users.noreply.github.com>
2021-07-24 00:56:30 -0500

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../lib/auth')
 
 const authController = require('../controllers/authcontroller')
 
+
+//common auth actions
 router.post('/signup', authController.signUp)
-
-
-router.post('/login',authController.login)
+router.post('/login', authController.login)
 router.get('/logout', authController.logout)
+
+// routes for recover user password
+router.post('/recover', authController.SendRecoverEmail)
+router.get('/resetpass/:iduser/:token', authController.RenderRecoverView)
+router.post('/resetpass/:iduser/:token', authController.ChangeRecoverPass)
 module.exports = router
