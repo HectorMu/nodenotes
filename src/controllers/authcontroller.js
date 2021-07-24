5f3704fe7d6a8e9f1095ba51fefeb59b09d585ee
HectorMu <42363909+HectorMu@users.noreply.github.com>
2021-07-24 00:55:23 -0500

    Update authcontroller.js

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -1,8 +1,10 @@
 const authController = {}
+const Dbpool = require('../database')
+const helpers = require('../lib/helpers')
+const jwt = require('jsonwebtoken')
 const { urlencoded } = require('express')
+const nodeMailer = require('../lib/nodemailer')
 const passport = require('passport')
-
-
 
 authController.signUp = passport.authenticate('signup',{
     successRedirect:'/profile',
@@ -24,17 +26,63 @@ authController.login = (req, res, next)=>{
         })
     })(req, res, next)
 }
-
 // authController.login = passport.authenticate('local.login',{
 //       successRedirect:'/profile',
 //       failureRedirect:'/login',
 //       failureFlash:true
 // })
-
-
-
 authController.logout = (req, res, next)=>{
     req.logOut();
     res.redirect('/')
 }
+
+
+//recover user password
+//this controller sends an email to a user to recover his password, user must be registered in the db
+authController.SendRecoverEmail = async(req, res)=>{
+    const{email} = req.body
+    const rows = await Dbpool.query("select * from users where email = ?",[email])
+    if(rows.length>0){
+        nodeMailer.Send(req, res);
+
+    }else{
+        req.flash("error_msg", "There isn't an account with that email")    
+        res.redirect('/recover')
+    }
+}
+//this controller render the recover view, need to receive the req.params to get it on the form action
+authController.RenderRecoverView = (req, res)=>{
+    const {token, iduser} = req.params
+    console.log(iduser)
+    try {
+        const payload = jwt.verify(token,process.env.TOKEN_SECRET)
+        res.render('resetpass',{
+            //here we pass the req.params to recover view
+            token, iduser
+        })
+    } catch (error) {
+        req.flash("error_msg", "Invalid provided token, the token has expired")    
+        res.redirect('/login')
+    }
+ }
+ //this controller change the password on post, first verify the token, then get if there is an user with that id, and
+ //finishing updating the user password
+ authController.ChangeRecoverPass = async (req, res)=>{
+    const {token, iduser} = req.params
+    const {newpass} = req.body
+    try {
+        const payload = jwt.verify(token,process.env.TOKEN_SECRET)
+        const user = await Dbpool.query("select * from users where iduser =  ?",[iduser])
+        console.log(user)
+        if(user.length>0){
+            const NewPassHash = await helpers.encryptPass(newpass)
+            await Dbpool.query("update users set pass = ? where iduser = ?",[NewPassHash,iduser])
+            req.flash("success_msg","Password updated successfully")
+            res.redirect('/login')
+        }
+    } catch (error) {
+        req.flash("error_msg", "Invalid provided token, the token has expired")    
+        res.redirect('/login')
+    }
+ }
 module.exports = authController
