cd2ef873c74014eeefd695edeab42f51f7dcf0db
HectorMu <42363909+HectorMu@users.noreply.github.com>
2021-07-27 11:02:46 -0500

    Update nodemailer.js

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -23,7 +23,7 @@ Email.Send = async (req, res)=>{
         id: user[0].iduser
     }
     const token = jwt.sign(payload,process.env.TOKEN_SECRET,{expiresIn: '15m'})
-    const link = `https://hmnodenotes.herokuapp.com/resetpass/${user[0].iduser}/${token}`
+    const link = `https://hmnodenotes.herokuapp.com/resetpass/${token}ñ${user[0].iduser}`
 
     let emailOptions = {
         from: 'nodenotes@outlook.com',
