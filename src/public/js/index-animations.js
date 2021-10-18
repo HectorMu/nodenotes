118a24376862b8470fed51c1e6721097139dabbd
Héctor Manuel Muñoz Flores <skyspartanblack@gmail.com>
2021-10-18 16:32:01 -0500

    Fully rework of the landing page design and of the colors

--- src/public/js/index-animations.js
+++ src/public/js/index-animations.js
@@ -1,31 +1,20 @@
 window.sr = ScrollReveal();
-window.addEventListener('load',()=>{
 
 sr.reveal('.header-content-left',{
-    duration: 2000,
+    duration: 1000,
     origin: 'top',
-    distance: '300px'
+    distance: '50px'
 });
-sr.reveal('.header-content-right',{
-    duration: 2000,
+sr.reveal('#header-card',{
+    duration: 1000,
     origin: 'right',
-    distance: '10px'
+    distance: '50px'
 });
-sr.reveal('#contact-description-column',{
-    duration: 2000,
-    origin: 'right',
-    distance:'10px'
-});
-sr.reveal('#howtostart',{
-    duration: 1500,
-    origin: 'top',
-    distance: '200px'
-});
-sr.reveal('#testimonial',{
-    duration: 2000,
-    origin: 'bottom',
-    distance: '300px'
 
+sr.reveal('#cardsrow',{
+    duration: 1000,
+    origin: 'right',
+    distance: '50px'
 });
 sr.reveal('#col-content-right',{
     duration: 2000,
@@ -33,14 +22,43 @@ sr.reveal('#col-content-right',{
     distance: '300px'
 
 });
+sr.reveal('#stepone-left',{
+    duration: 1500,
+    origin: 'left',
+    distance:'50px'
+});
+sr.reveal('#stepone-right',{
+    duration: 1500,
+    origin: 'right',
+    distance:'50px'
+});
+
+sr.reveal('#steptwo-left',{
+    duration: 1500,
+    origin: 'left',
+    distance:'50px'
+});
+sr.reveal('#steptwo-right',{
+    duration: 1500,
+    origin: 'right',
+    distance:'50px'
+});
+
+
+sr.reveal('#stepthree-left',{
+    duration: 1500,
+    origin: 'left',
+    distance:'50px'
+});
+sr.reveal('#stepthree-right',{
+    duration: 1500,
+    origin: 'right',
+    distance:'50px'
+});
+
 sr.reveal('#contact-column',{
     duration: 2000,
     origin: 'left',
-    distance:'10px'
-});
-sr.reveal('#contact-description-column',{
-    duration: 2000,
-    origin: 'right',
     distance:'10px'
 });
 sr.reveal('#backtop',{
@@ -51,7 +69,81 @@ sr.reveal('#backtop',{
 
 
 
-})
+
+
+sr.reveal('#circle1',{
+    duration: 500,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('#arrow1',{
+    delay:500,
+    duration: 500,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('#circle2',{
+    delay:1000,
+    duration: 500,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('#arrow2',{
+    delay:1500,
+    duration: 500,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('#circle3',{
+    delay:2000,
+    duration: 500,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('#check',{
+    delay:2500,
+    duration: 500,
+    origin: 'left',
+    distance: '50px'
+});
+
+
+sr.reveal('#card1',{
+    duration: 500,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('#card2',{
+    delay:500,
+    duration: 500,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('#card4',{
+    delay:1000,
+    duration: 500,
+    origin: 'top',
+    distance: '50px'
+});
+sr.reveal('#card3',{
+    delay:1500,
+    duration: 500,
+    origin: 'right',
+    distance: '50px'
+});
+sr.reveal('#formcard',{
+    duration: 500,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('#contactdiv',{
+    duration: 500,
+    origin: 'right',
+    distance: '50px'
+});
+
+
+
 
 
 
