74ddb7433ed67bf56fc4d489b8c23cef27237b90
Héctor Manuel Muñoz Flores <skyspartanblack@gmail.com>
2021-10-23 17:47:06 -0500

    Something fixed

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -6,9 +6,7 @@ controller.getAllRegisteredUsers = async(req, res) =>{
     try {
         const validLocations = ['/signup','/recover']
         const location = req.headers.location
-        console.log(location)
         if(location != undefined){
-            console.log(validLocations)
             if(location.includes(validLocations)){
                 const users = await connection.query('select email from users')
                 res.json(users);
